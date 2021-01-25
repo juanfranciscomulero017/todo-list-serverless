cfd4c8ae1b24369e33e917f274377f4c77913c75
juanfrancisco.mulero017@comunidadunir.net <ec2-user@ip-172-31-24-243.ec2.internal>
2021-01-25 00:31:09 +0000

    añadimos cambios de permisos en el serverless.yml en la nueva función de traducción

--- todos/translate.py
+++ todos/translate.py
@@ -1,33 +1,14 @@
 import boto3
-import json
-import os
 
-from todos import decimalencoder
 
 translate = boto3.client('translate')
 
-
-
-
-dynamodb = boto3.resource('dynamodb')
-
-
-def list(event, context):
-    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-
-    # fetch all todos from the database
-    result = table.scan()
-
-    # create a response
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(result['Items'], cls=decimalencoder.DecimalEncoder)
-    }
+cuerpo = "prueba"
     
-    cadena1_traducida = translate.translate_text(Text=response,
-                                          SourceLanguageCode="auto",
-                                                                            TargetLanguageCode="en")
+cadena1_traducida = translate.translate_text(Text=cuerpo,
+                                         SourceLanguageCode="auto",
+                                         TargetLanguageCode="en")
+                                        
+lenguaje = input(cadena1_traducida["TranslatedText"])
 
-    lenguaje = input(cadena1_traducida["TranslatedText"])
-
-    return lenguaje
+print(lenguaje)

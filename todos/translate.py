c9a6b657548efd7f6e2c83be74ee2681a05ecf7c
juanfrancisco.mulero017@comunidadunir.net <ec2-user@ip-172-31-24-243.ec2.internal>
2021-01-25 00:56:30 +0000

    añadimos cambios de permisos en el serverless.yml en la nueva función de traducción

--- todos/translate.py
+++ todos/translate.py
@@ -3,12 +3,20 @@ import boto3
 
 translate = boto3.client('translate')
 
+
+
 cuerpo = "prueba"
     
 cadena1_traducida = translate.translate_text(Text=cuerpo,
                                          SourceLanguageCode="auto",
                                          TargetLanguageCode="en")
                                         
-lenguaje = input(cadena1_traducida["TranslatedText"])
+lenguaje = (cadena1_traducida["TranslatedText"])
 
-print(lenguaje)
+response = {
+        "statusCode": 200,
+        "body": lenguaje
+}
+
+
+print(response)
